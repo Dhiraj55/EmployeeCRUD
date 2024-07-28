eb39a2b66e72be47385cdcda39c1bd62c4e765f5
Dhiraj Bodake <dhiraj.bodake.ext@siemens.com>
2024-07-28 15:07:08 +0530

    backend Setup

---
+++ ems-bakend/src/main/java/com/fullstack/ems/EmsBakendApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsBakendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsBakendApplication.class, args);
+	}
+
+}
